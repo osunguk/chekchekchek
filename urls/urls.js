cd65639798abbfc6c8ce01785d0fbc8841309acc
osunguk <osunguk@gmail.com>
2020-03-29 19:12:09 +0900

    add url export & add key

--- urls/urls.js
+++ urls/urls.js
@@ -1,7 +1,13 @@
 import key from '../conf/key'
 
-const searchBase = "http://book.interpark.com/api/search.api"
-const bestsellerBase = "http://book.interpark.com/api/bestSeller.api"
-const recommandBase = "http://book.interpark.com/api/recommend.api"
-const newBookBase = "http://book.interpark.com/api/newBook.api"
+const searchBase = "http://book.interpark.com/api/search.api"+"key="+key
+const bestsellerBase = "http://book.interpark.com/api/bestSeller.api"+"key="+key
+const recommandBase = "http://book.interpark.com/api/recommend.api"+"key="+key
+const newBookBase = "http://book.interpark.com/api/newBook.api"+"key="+key
 
+export {
+    searchBase,
+    bestSeller,
+    recommandBase,
+    newBookBase
+}
