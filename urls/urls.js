21081edc3176cc363ff558aa5eed0b2a9eb909ed
osunguk <osunguk@gmail.com>
2020-03-29 18:12:22 +0900

    Add api urls

---
+++ urls/urls.js
@@ -0,0 +1,7 @@
+import key from '../conf/key'
+
+const searchBase = "http://book.interpark.com/api/search.api"
+const bestsellerBase = "http://book.interpark.com/api/bestSeller.api"
+const recommandBase = "http://book.interpark.com/api/recommend.api"
+const newBookBase = "http://book.interpark.com/api/newBook.api"
+
