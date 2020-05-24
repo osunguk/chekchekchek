4473f1af16709471442e66621ac905a6cd224a0a
osunguk(mac) <osunguk@gmail.com>
2020-05-24 20:38:08 +0900

    포트 변경

--- backend/app.js
+++ backend/app.js
@@ -1,9 +1,7 @@
 const express = require('express')
 const app = express()
-const port = 3000
+const port = 3001
 
 app.get('/', (req,res) => res.send('hello world!'))
 
 app.listen(port, () => console.log(`Sucess Example app listening on port ${port}`))
-
-
